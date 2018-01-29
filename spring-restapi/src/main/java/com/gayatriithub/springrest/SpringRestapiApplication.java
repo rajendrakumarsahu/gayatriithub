ed547a6fbca8c4ae66c18d6d8c4e69cd01b4f993
Lohitansh <Lohitansh@LAPTOP-OEQIVJL7>
2018-01-29 21:16:43 +0530

    comment

---
+++ spring-restapi/src/main/java/com/gayatriithub/springrest/SpringRestapiApplication.java
@@ -0,0 +1,12 @@
+package com.gayatriithub.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiApplication.class, args);
+	}
+}
